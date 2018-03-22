c6394c3cab044405b7464757fababb842b34958f
Casey Dallavalle <cbdallavalle@gmail.com>
2018-03-22 10:06:02 -0600

    add .catch after fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,7 +15,7 @@ this.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
-    })
+    }).catch(error => console.log(error.message))
   );
 });
 
